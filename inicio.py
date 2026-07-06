bd92837116e040d67e548bd9702917609c10c2a3
sa3hyun <genesis.trincado@outlook.cl>
2026-07-06 11:43:11 -0400

    Add files via upload

--- inicio.py
+++ inicio.py
@@ -6,6 +6,6 @@ df = pd.read_csv('piv.csv')
 
 st.sidebar.header("Filtros de Búsqueda")
 with st.sidebar:
-    st.markdown("###Dashboard Interactivo: Crecimiento del PIB (% anual)")
+    st.markdown("### Dashboard Interactivo: Crecimiento del PIB (% anual)")
 
 st.subheader("made by sa3hyun", text_alignment="center")
