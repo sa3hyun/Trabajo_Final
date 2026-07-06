d5c0a0d1bba1471103ac1c70a37a56875b61d521
sa3hyun <genesis.trincado@outlook.cl>
2026-07-06 11:35:26 -0400

    Add files via upload

--- inicio.py
+++ inicio.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.sidebar.header("Filtros de Búsqueda")
+with st.sidebar:
+    st.markdown("### Dashboard Interactivo:<br>Crecimiento del PIB (% anual)", unsafe_allow_html=True)
+    st.write("---")
 
 st.subheader("made by sa3hyun", text_alignment="center")
