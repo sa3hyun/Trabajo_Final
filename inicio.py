69f7f720971e884f68105c72f8a2ec4ff91b50ab
sa3hyun <genesis.trincado@outlook.cl>
2026-07-06 11:36:26 -0400

    Add files via upload

--- inicio.py
+++ inicio.py
@@ -4,7 +4,6 @@ import numpy as np
 
 st.sidebar.header("Filtros de Búsqueda")
 with st.sidebar:
-    st.markdown("### Dashboard Interactivo:<br>Crecimiento del PIB (% anual)", unsafe_allow_html=True)
-    st.write("---")
+    st.markdown("### Dashboard Interactivo:<br>Crecimiento del PIB (% anual)")
 
 st.subheader("made by sa3hyun", text_alignment="center")
