e7fe227b6d1874385cc8685f4e916e19057c928c
sa3hyun <genesis.trincado@outlook.cl>
2026-07-06 11:50:59 -0400

    Add files via upload

--- inicio.py
+++ inicio.py
@@ -1,12 +1,11 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+
 
 df = pd.read_csv("pib.csv")
 
 st.sidebar.header("Filtros de Búsqueda")
-lista_paises = sorted(df['Country Name'].unique())
-pais = st.sidebar.selectbox("Selecciona un País:", lista_paises)
+
 with st.sidebar:
     st.markdown("### Dashboard Interactivo: Crecimiento del PIB (% anual)")
 
