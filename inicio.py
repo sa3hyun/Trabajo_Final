f6a7b9dd9c4cea96fac165d6350d0e1abdc45b53
sa3hyun <genesis.trincado@outlook.cl>
2026-07-06 11:42:41 -0400

    Add files via upload

--- inicio.py
+++ inicio.py
@@ -2,8 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+df = pd.read_csv('piv.csv')
+
 st.sidebar.header("Filtros de Búsqueda")
 with st.sidebar:
-    st.markdown("### Dashboard Interactivo: Crecimiento del PIB (% anual)")
+    st.markdown("###Dashboard Interactivo: Crecimiento del PIB (% anual)")
 
 st.subheader("made by sa3hyun", text_alignment="center")
