86464dca71472213799d0bd6addca028157aa7a8
sa3hyun <genesis.trincado@outlook.cl>
2026-07-08 16:10:29 -0400

    Add files via upload

--- inicio.py
+++ inicio.py
@@ -5,7 +5,7 @@ import pandas as pd
 df = pd.read_csv("nuevo_pib.csv")
 
 st.image("https://www.biobiochile.cl/assets/logos-notas-patrocinadas/6302219_logo_img_patrocinada.png")
-st.header("Introduccion a la programacion con R y Python")
+st.subheader("Introduccion a la programacion con R y Python")
 
 
 st.sidebar.header("Filtros de Búsqueda")
