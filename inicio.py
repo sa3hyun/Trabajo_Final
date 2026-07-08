b88c66d0d68cacff4894791d0f5203367392c203
sa3hyun <genesis.trincado@outlook.cl>
2026-07-08 16:27:53 -0400

    Add files via upload

--- inicio.py
+++ inicio.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 
 df = pd.read_csv("nuevo_pib.csv")
@@ -11,10 +12,16 @@ st.subheader("Introduccion a la programacion con R y Python")
 st.sidebar.header("Filtros de Búsqueda")
 
 paises = df['Country Name']
-st.write(paises)
+#st.write(paises)
 
 with st.sidebar:
     st.markdown("Dashboard Interactivo: Crecimiento del PIB (% anual)")
-    st.selectbox('escoger pais', paises)
+    pais = st.selectbox('Escoger pais', paises)
+    st.markdown('---')
+    st.write('Génesis Trincado Campos')
+    st.write(pais)
+
+df= pd.DataFrame(np.random.randn(10, 2),columns=['x', 'y'])
+st.line_chart(pais)
 
 st.link_button("Made by @Sa3hyun", "https://github.com/sa3hyun")
