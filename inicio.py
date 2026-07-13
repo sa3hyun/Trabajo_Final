af156a90eea8e44af2fd6f32e596b5f12faa5b08
sa3hyun <genesis.trincado@outlook.cl>
2026-07-13 12:00:46 -0400

    Add files via upload

--- inicio.py
+++ inicio.py
@@ -12,19 +12,20 @@ st.subheader("Introduccion a la programacion con R y Python")
 st.sidebar.header("Filtros de Búsqueda")
 
 paises = df['Country Name']
-año = df[['1961', '1962', '1963', '1964', '1965', '1975', '1976', '1977', '1978', '1979', '1980', '1981', '1982', '1983', '1984', '1985', '1986', '1987', '1988', '1989', '1990', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024', '2025']]
-st.write(paises)
 
 with st.sidebar:
     st.markdown("Dashboard Interactivo: Crecimiento del PIB (% anual)")
-    st.selectbox('escoger pais', paises)
+    pais = st.selectbox('escoger pais', paises)
     st.markdown("---")
     st.write("- Génesis Trincado")
+
+st.write (pais)
+anios = st.slider('Elige Año', 1961, 2025, value=(1961, 2025)) 
 
 df= pd.DataFrame(np.random.randn(10, 2),columns=['x', 'y'])
 st.line_chart(df)
 
-#st.slider('Elige Año', año) 
+
 
 
 
